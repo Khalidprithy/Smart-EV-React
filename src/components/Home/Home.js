3a5ebe24c7f886d7af320c4a4b078142e199a4f8
Khalidprithy <khalidprithy@gmail.com>
2022-04-04 03:38:48 +0600

    review component card showed

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,22 +1,15 @@
-import React, { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import useReviews from '../../hooks/useReviews';
 import HomeIntro from '../HomeIntro/HomeIntro';
 import HomeReviews from '../HomeReviews/HomeReviews';
 import Review from '../Review/Review';
 import './Home.css'
 
 const Home = () => {
-    const [reviews, setReviews] = useState([]);
-    console.log(reviews);
-
-    useEffect(() => {
-        fetch('review.json')
-            .then(res => res.json())
-            .then(data => setReviews(data))
-    }, [])
-
-
+    const navigate = useNavigate()
+    const [reviews, setReviews] = useReviews()
     return (
-        <div>
+        <div className='home-container'>
             <HomeIntro></HomeIntro>
             <h4 className='user-review-title'>User Reviews</h4>
             <div className='home-reviews'>
@@ -27,15 +20,9 @@ const Home = () => {
                     ></HomeReviews>)
                 }
             </div>
-            <div>
-                {
-                    reviews.map(review => <Review
-                        key={review.id}
-                        review={review}
-                    ></Review>)
-                }
+            <div className='show-btn-container'>
+                <button onClick={() => navigate('/review')} className='show-all-btn'>Show All Reviews</button>
             </div>
-
         </div>
     );
 };
--- src/components/Review/Review.js
+++ src/components/Review/Review.js
@@ -1,10 +1,25 @@
 import React from 'react';
+import './Review.css'
+import useReviews from '../../hooks/useReviews';
 
-const Review = (props) => {
-    console.log(props);
+const Review = () => {
+    const [reviews, setReviews] = useReviews();
+
     return (
         <div>
-            <h4>Review</h4>
+            {
+                reviews.map(review => (
+                    <div className='review-card-compo'>
+                        <div className='user-info'>
+                            <img src={review.image} alt="" />
+                            <h6>{review.name}</h6>
+                        </div>
+                        <p><small className='review-text'>{review.text}</small></p>
+                        <p className='ratings'>Rating: {review.rating}</p>
+                    </div>
+
+                ))
+            }
         </div>
     );
 };
